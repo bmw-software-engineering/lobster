f774a15213bb3d195305c4a6d0f84e5467c8433b
Florian Schanda <florian.schanda@bmw.de>
2023-04-12 11:16:34 +0200

    Factor out gtest include for lobster

--- tests/projects/basic/test.cpp
+++ tests/projects/basic/test.cpp
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
-#include "lobster.h"
+#include <lobster_gtest.h>
 #include "foo.h"
 
 TEST(ImplicationTest, BasicTest)
 {
-  LOBSTER_TRACING("example.req_implication");
+  LOBSTER_TRACE("example.req_implication");
 
   EXPECT_TRUE(implication(false, true));
   EXPECT_TRUE(implication(true, true));
