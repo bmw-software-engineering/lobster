6d75f0fce2d56a09ad650457bcf60683d26bd3d4
Florian Schanda <florian.schanda@bmw.de>
2023-10-24 09:59:30 +0200

    Add unit test for JSON name synthesis

---
+++ test-unit/test_json.py
@@ -0,0 +1,33 @@
+import unittest
+import re
+from pathlib import PurePosixPath, PureWindowsPath
+
+from lobster.tools.json import json
+
+
+class Test_Json(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testSynName(self):
+        self.assertEqual(
+            json.syn_test_name(PurePosixPath("foo/bar.json")),
+            "foo.bar")
+        self.assertEqual(
+            json.syn_test_name(PurePosixPath("/foo/bar.json")),
+            "foo.bar")
+        self.assertEqual(
+            json.syn_test_name(PureWindowsPath("foo\\bar.json")),
+            "foo.bar")
+        self.assertEqual(
+            json.syn_test_name(PureWindowsPath("C:\\foo\\bar.json")),
+            "foo.bar")
+        self.assertEqual(
+            json.syn_test_name(PurePosixPath("../../foo/./bar.json")),
+            "foo.bar")
+        self.assertEqual(
+            json.syn_test_name(PureWindowsPath("..\\..\\foo\\.\\bar.json")),
+            "foo.bar")
