b14524664931678f56b80d665e15f7fbf6dde000
christophkloeffel <christoph.kloeffel@accenture.com>
2024-06-14 15:38:20 +0200

    removes invalid email address and replaces it with a valid one

--- packages/lobster-tool-cpp/setup.py
+++ packages/lobster-tool-cpp/setup.py
@@ -48,7 +48,7 @@ setuptools.setup(
     name="bmw-lobster-tool-cpp",
     version=version.LOBSTER_VERSION,
     author="Bayerische Motoren Werke Aktiengesellschaft (BMW AG)",
-    author_email="florian.schanda@bmw.de",
+    author_email="philipp.wullstein-kammler@bmw.de",
     description="LOBSTER Tool for ISO C/C++",
     long_description=long_description,
     long_description_content_type="text/markdown",
