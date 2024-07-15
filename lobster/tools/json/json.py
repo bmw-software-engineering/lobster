8bdbde4e3c2fe4aed89e8e95a15d36b84fe28933
christophkloeffel <christoph.kloeffel@accenture.com>
2024-07-15 18:28:00 +0200

    includes file name in tag of json-tests

--- lobster/tools/json/json.py
+++ lobster/tools/json/json.py
@@ -174,7 +174,8 @@ class LOBSTER_Json(LOBSTER_Per_File_Tool):
 
                 l_item = Activity(
                     tag       = Tracing_Tag(namespace = "json",
-                                            tag       = item_name),
+                                            tag       = "%s:%s" %
+                                            (file_name, item_name)),
                     location  = File_Reference(file_name),
                     framework = "JSON",
                     kind      = "Test Vector")
