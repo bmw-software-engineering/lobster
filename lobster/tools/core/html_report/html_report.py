4794986f390a500c22a89d66003132ad6150584b
Kedar Navare <kedar.navare@techworks.bmwgroup.com>
2024-12-10 15:02:56 +0100

    removed unused ast import

--- lobster/tools/core/html_report/html_report.py
+++ lobster/tools/core/html_report/html_report.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Affero General Public
 # License along with this program. If not, see
 # <https://www.gnu.org/licenses/>.
-import ast
 import os.path
 import argparse
 import html
