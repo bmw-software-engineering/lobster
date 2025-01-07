f3ddc626ae2bdf17dd9b3146496676f2f0f1bc82
Kedar Navare <kedar.navare@techworks.bmwgroup.com>
2025-01-07 07:07:02 +0100

    Dynamically update the reference output file in test-integration basic and filter tests

---
+++ tests-utils/update_online_json_with_hashes.py
@@ -0,0 +1,34 @@
+import json
+import subprocess
+import sys
+from fileinput import filename
+
+
+# File name
+# file_name = 'expected-output.lobster'
+
+def update_json(filename):
+    # Load the JSON data from the file
+    with open(filename, 'r') as file:
+        data = json.load(file)
+
+    # Traverse the JSON structure and update the 'loc' values
+    for level in data['levels']:
+        for item in level['items']:
+            location = item['location']
+            if 'file' in location:
+                exec_commit_id = subprocess.check_output(
+                    ["git", "log", "-n", "1", "--format=%H", "--",
+                     "../../../"+location['file']]).decode().strip()
+                location['exec_commit_id'] = exec_commit_id
+
+    # Save the updated JSON data back to the same file
+    with open(filename, 'w') as fd:
+        json.dump(data, fd, indent=2)
+        fd.write("\n")
+
+    print(f"JSON data updated and saved back to '{filename}'.")
+
+
+if __name__ == "__main__":
+    update_json(sys.argv[1])
