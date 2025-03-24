2409cc869e41011fcdd3f40563557ae217a9c00b
SurajBDeore <suraj.da.deore@partner.bmwgroup.com>
2025-03-24 18:08:06 +0530

    Implemented STF for lobster-trlc (#246)
    System test infrastructure is implemented for lobster-trlc.
    6 system test cases added.
    
     Co-authored-by: Kedar Navare <kedar.na.navare@partner.bmwgroup.com>

---
+++ tests-system/lobster-trlc/test_input_list_of_files.py
@@ -0,0 +1,42 @@
+from .lobster_system_test_case_base import LobsterTrlcSystemTestCaseBase
+from ..asserter import Asserter
+
+
+class InputListOfFilesTest(LobsterTrlcSystemTestCaseBase):
+    def setUp(self):
+        super().setUp()
+        self._test_runner = self.create_test_runner()
+        self._test_runner.declare_input_file(self._data_directory / "default_file.rsl")
+        self._test_runner.declare_input_file(self._data_directory / "default_file.trlc")
+        self._test_runner.declare_trlc_config_file(self._data_directory /
+                                                   "lobster-trlc.conf")
+
+    def test_input_files_list(self):
+        OUT_FILE = "input_files_list.lobster"
+        self._test_runner.cmd_args.out = OUT_FILE
+        self._test_runner.declare_output_file(self._data_directory / OUT_FILE)
+        completed_process = self._test_runner.run_tool_test()
+        asserter = Asserter(self, completed_process, self._test_runner)
+        asserter.assertNoStdErrText()
+        asserter.assertStdOutText(f"lobster-trlc: successfully wrote 1 items to "
+                                  f"{OUT_FILE}\n")
+        asserter.assertExitCode(0)
+        asserter.assertOutputFiles()
+
+    def test_duplicate_input_files_list(self):
+        self._test_runner.declare_input_file(self._data_directory /
+                                             "default_file_copy.rsl")
+        self._test_runner.declare_input_file(self._data_directory /
+                                             "default_file_copy.trlc")
+        completed_process = self._test_runner.run_tool_test()
+        asserter = Asserter(self, completed_process, self._test_runner)
+        asserter.assertNoStdErrText()
+        asserter.assertStdOutText('package test_default\n        ^^^^^^^^^^^^ '
+                                  'default_file_copy.rsl:1: error: duplicate '
+                                  'definition, previous definition at '
+                                  'default_file.rsl:1\nnamaste goodname '
+                                  '{\n        ^^^^^^^^ default_file_copy.trlc:3: '
+                                  'error: duplicate definition, previous definition'
+                                  ' at default_file.trlc:3\nlobster-trlc: aborting'
+                                  ' due to earlier error\n')
+        asserter.assertExitCode(1)
