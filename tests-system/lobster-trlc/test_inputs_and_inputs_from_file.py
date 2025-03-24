90c1125303ab92a414c7a9cff1dccdcb5cf3cfb8
kedarnn <kedar.na.navare@partner.bmwgroup.com>
2025-03-24 21:18:09 +0530

    Added a test case for combination of inputs and inputs from file (#247)
    System testcase for lobster-trlc added. The test case covers a happy
    scenario when both the `inputs` and `inputs_from_file` options are
    given.

---
+++ tests-system/lobster-trlc/test_inputs_and_inputs_from_file.py
@@ -0,0 +1,28 @@
+from .lobster_system_test_case_base import LobsterTrlcSystemTestCaseBase
+from ..asserter import Asserter
+
+
+class InputFromFilesAndInputsTest(LobsterTrlcSystemTestCaseBase):
+    def setUp(self):
+        super().setUp()
+        self._test_runner = self.create_test_runner()
+        self._test_runner.declare_input_file(self._data_directory / "default_file.rsl")
+        self._test_runner.declare_input_file(self._data_directory / "default_file.trlc")
+        self._test_runner.declare_trlc_config_file(
+            self._data_directory / "inputs-from-files-and-inputs.conf")
+
+    def test_input_from_files_and_inputs_list(self):
+        # lobster-trace: trlc_req.Input_list_Of_File_And_Inputs_From_File
+        OUT_FILE = "input_from_files_and_inputs.lobster"
+        self._test_runner.cmd_args.out = OUT_FILE
+        self._test_runner.declare_output_file(self._data_directory / OUT_FILE)
+        self._test_runner.declare_inputs_from_file(self._data_directory /
+                                                   "input_from_files_and_inputs.txt",
+                                                   self._data_directory)
+        completed_process = self._test_runner.run_tool_test()
+        asserter = Asserter(self, completed_process, self._test_runner)
+        asserter.assertNoStdErrText()
+        asserter.assertStdOutText(f"lobster-trlc: successfully wrote 2 items to "
+                                  f"{OUT_FILE}\n")
+        asserter.assertExitCode(0)
+        asserter.assertOutputFiles()
