ee0746b2eeccf0347e818f147144099ad7a355ef
SurajBDeore <suraj.da.deore@partner.bmwgroup.com>
2025-03-24 18:08:06 +0530

    Implemented STF for lobster-trlc (#246)
    System test infrastructure is implemented for lobster-trlc.
    6 system test cases added.
    
     Co-authored-by: Kedar Navare <kedar.na.navare@partner.bmwgroup.com>

---
+++ tests-system/lobster-trlc/lobster_trlc_test_runner.py
@@ -0,0 +1,94 @@
+from subprocess import CompletedProcess
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Optional
+import yaml
+from ..test_runner import TestRunner
+
+
+@dataclass
+class ConfigFileData:
+    inputs: List[str] = None
+    trlc_config_file : Optional[str] = None
+    inputs_from_file: Optional[str] = None
+    traverse_bazel_dirs : Optional[str] = None
+
+    def __post_init__(self):
+        self.inputs = []
+
+    def dump(self, filename: str):
+        data = {}
+
+        def append_if_not_none(key, value):
+            if value is not None:
+                data[key] = value
+
+        append_if_not_none("inputs", self.inputs)
+        append_if_not_none("inputs_from_file", self.inputs_from_file)
+        append_if_not_none("trlc_config_file", self.trlc_config_file)
+        append_if_not_none("traverse_bazel_dirs", self.traverse_bazel_dirs)
+
+        with open(filename, mode='w', encoding="UTF-8") as file:
+            yaml.dump(data, file)
+
+
+@dataclass
+class CmdArgs:
+    config: Optional[str] = "config.yaml"
+    out: Optional[str] = None
+
+    def as_list(self) -> List[str]:
+        """Returns the command line arguments as a list"""
+        cmd_args = []
+
+        def append_if_string(parameter: str, value: Optional[str]):
+            if value is not None:
+                cmd_args.append(f"{parameter}={value}")
+
+        append_if_string("--out", self.out)
+        append_if_string("--config", self.config)
+        return cmd_args
+
+
+class LobsterTrlcTestRunner(TestRunner):
+    """System test runner for lobster-TRLC"""
+    def __init__(self, tool_name: str, working_dir: Path):
+        super().__init__(tool_name, working_dir)
+        self._config_file_data = ConfigFileData()
+        self._cmd_args = CmdArgs()
+
+    @property
+    def cmd_args(self) -> CmdArgs:
+        return self._cmd_args
+
+    @property
+    def config_file_data(self) -> ConfigFileData:
+        return self._config_file_data
+
+    def declare_input_file(self, file: Path):
+        super().declare_input_file(file)
+        self.config_file_data.inputs.append(file.name)
+
+    def declare_trlc_config_file(self, file: Path):
+        super().declare_input_file(file)
+        self.config_file_data.trlc_config_file = file.name
+
+    def declare_inputs_from_file(self, file: Path, data_directory: Path):
+        super().declare_input_file(file)
+        self.config_file_data.inputs_from_file = file.name
+        # TODO
+        #  This will only work for files and not for directories.
+        #  Traversing directories logic needs to be added
+        with open(file, "r", encoding="UTF-8") as fd:
+            for line in fd:
+                super().declare_input_file(data_directory / line.strip())
+
+    def get_tool_args(self) -> List[str]:
+        """Returns the command line arguments that shall be used to start 'lobster-trlc'
+           under test"""
+        return self._cmd_args.as_list()
+
+    def run_tool_test(self) -> CompletedProcess:
+        if self._cmd_args.config:
+            self._config_file_data.dump(str(self._working_dir / self._cmd_args.config))
+        return super().run_tool_test()
---
+++ tests-system/lobster-trlc/test_inputs_from_file.py
@@ -0,0 +1,42 @@
+from .lobster_system_test_case_base import LobsterTrlcSystemTestCaseBase
+from ..asserter import Asserter
+
+
+class InputFromFilesTest(LobsterTrlcSystemTestCaseBase):
+    def setUp(self):
+        super().setUp()
+        self._test_runner = self.create_test_runner()
+        self._test_runner.declare_trlc_config_file(self._data_directory /
+                                                   "lobster-trlc.conf")
+
+    def test_input_from_files(self):
+        OUT_FILE = "input_from_files.lobster"
+        self._test_runner.cmd_args.out = OUT_FILE
+        self._test_runner.declare_output_file(self._data_directory / OUT_FILE)
+        self._test_runner.declare_inputs_from_file(self._data_directory /
+                                                   "valid_ext_inputs_from_file.txt",
+                                                   self._data_directory)
+        completed_process = self._test_runner.run_tool_test()
+        asserter = Asserter(self, completed_process, self._test_runner)
+        asserter.assertNoStdErrText()
+        asserter.assertStdOutText(f"lobster-trlc: successfully wrote 1 items to "
+                                  f"{OUT_FILE}\n")
+        asserter.assertExitCode(0)
+        asserter.assertOutputFiles()
+
+    def test_input_from_files_duplicate_contents(self):
+        self._test_runner.declare_inputs_from_file(self._data_directory /
+                                                   "input_from_file_duplicate_data.txt",
+                                                   self._data_directory)
+        completed_process = self._test_runner.run_tool_test()
+        asserter = Asserter(self, completed_process, self._test_runner)
+        asserter.assertNoStdErrText()
+        asserter.assertStdOutText('package test_default\n        ^^^^^^^^^^^^ '
+                                  'default_file_copy.rsl:1: error: duplicate '
+                                  'definition, previous definition at '
+                                  'default_file.rsl:1\nnamaste goodname '
+                                  '{\n        ^^^^^^^^ default_file_copy.trlc:3: '
+                                  'error: duplicate definition, previous definition'
+                                  ' at default_file.trlc:3\nlobster-trlc: aborting'
+                                  ' due to earlier error\n')
+        asserter.assertExitCode(1)
