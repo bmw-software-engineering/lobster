3c01c33083095eac4a415d922895298b292b46ee
mugdhadhole1 <161293900+mugdhadhole1@users.noreply.github.com>
2025-03-26 14:48:16 +0530

    Migration - lobster-report system tests to new STF (#237)
    Migrated following lobster-report system tests to STF 
    
    - Missing
    - Justified
    - Ok - made changes in OK status system test

--- tests-system/lobster-report/test_status_justified.py
+++ tests-system/lobster-report/test_status_justified.py
@@ -2,22 +2,22 @@ from .lobster_report_system_test_case_base import LobsterReportSystemTestCaseBas
 from ..asserter import Asserter
 
 
-class ReportOkTest(LobsterReportSystemTestCaseBase):
+class ReportJustifiedTest(LobsterReportSystemTestCaseBase):
     def setUp(self):
         super().setUp()
         self._test_runner = self.create_test_runner()
 
-    def test_status_ok(self):
-        # lobster-trace: core_report_req.Status_Ok
+    def test_status_justified(self):
+        # lobster-trace: core_report_req.Status_Justified_Global
         self._test_runner.declare_input_file(self._data_directory /
-                                             "lobster_ok.conf")
+                                             "lobster_justified.conf")
         self._test_runner.declare_input_file(self._data_directory /
-                                             "trlc_ok.lobster")
+                                             "trlc_justified.lobster")
         self._test_runner.declare_input_file(self._data_directory /
-                                             "python_ok.lobster")
+                                             "python_justified.lobster")
 
-        conf_file = "lobster_ok.conf"
-        out_file = "report_ok.lobster"
+        conf_file = "lobster_justified.conf"
+        out_file = "report_justified.lobster"
         self._test_runner.cmd_args.lobster_config = conf_file
         self._test_runner.cmd_args.out = out_file
         self._test_runner.declare_output_file(self._data_directory / out_file)
