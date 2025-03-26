6681ef22c2faef0a2f5e65c63191cea513928950
Philipp Wullstein-Kammler <philipp.wullstein-kammler@bmw.de>
2025-03-26 15:00:50 +0100

    Fix BMW copyright dates
    Adjusted the BMW copyright dates of every `*.py` file
    according to its git history, if that file has a copyright disclaimer.

--- lobster/tools/core/online_report/online_report.py
+++ lobster/tools/core/online_report/online_report.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # lobster_online_report - Transform file references to github references
-# Copyright (C) 2023 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+# Copyright (C) 2023-2025 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
