26d08c785d0d8969dbd403de60d463593698417f
Philipp Wullstein-Kammler <philipp.wullstein-kammler@bmw.de>
2025-03-27 17:31:37 +0100

    Move authentication provision into separate function
    Extract the creation of the authentication data from `query_cb_single`
    and move it into a separate function.
    
    Clean up import statements.
    
    Move class `BearerAuth` into separate file.

---
+++ lobster/tools/codebeamer/bearer_auth.py
@@ -0,0 +1,10 @@
+from requests.auth import AuthBase
+
+
+class BearerAuth(AuthBase):
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        r.headers['Authorization'] = f'Bearer {self.token}'
+        return r
---
+++ tests-unit/lobster-codebeamer/test_authentication.py
@@ -0,0 +1,46 @@
+import unittest
+from requests.auth import HTTPBasicAuth
+from lobster.tools.codebeamer.bearer_auth import BearerAuth
+from lobster.tools.codebeamer.codebeamer import get_authentication
+from lobster.tools.codebeamer.config import AuthenticationConfig, Config
+
+class AuthenticationTest(unittest.TestCase):
+
+    _USERS = [None, "Procyon"]
+    _PASSWORDS = [None, "Andromeda"]
+
+    def test_get_bearer_auth(self):
+        # This test verifies that the bearer authentication always takes precedence over
+        # basic authentication.
+
+        for password in self._PASSWORDS:
+            for user in self._USERS:
+                with self.subTest(user=user, password=password):
+                    cb_auth_conf=AuthenticationConfig(
+                        token="local bubble",
+                        user=None,
+                        password=None,
+                        root="milky way",
+                    )
+                    auth = get_authentication(cb_auth_conf)
+                    self.assertIsInstance(auth, BearerAuth)
+    
+    def test_get_basic_auth(self):
+        # This test verifies that the basic authentication is returned,
+        # even if the user name and/or password are missing.
+
+        for password in self._PASSWORDS:
+            for user in self._USERS:
+                with self.subTest(user=user, password=password):
+                    cb_auth_conf=AuthenticationConfig(
+                        token=None,
+                        user=user,
+                        password=password,
+                        root="orion arm",
+                    )
+                    auth = get_authentication(cb_auth_conf)
+                    self.assertIsInstance(auth, HTTPBasicAuth)
+
+
+if __name__ == '__main__':
+    unittest.main()
