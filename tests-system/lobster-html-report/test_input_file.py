76d057276e6295c4b80aa036c0d88e838cac9ca7
Suraj Deore <suraj.da.deore@partner.bmwgroup.com>
2025-05-05 17:22:25 +0530

    review changes

--- tests-system/lobster-html-report/test_input_file.py
+++ tests-system/lobster-html-report/test_input_file.py
@@ -1,8 +1,9 @@
 from .lobster_UI_system_test_case_base import LobsterUISystemTestCaseBase
 from ..asserter import Asserter
+from lobster.tools.core.html_report.html_report import is_dot_available
 
 
-class TestLobsterHtmlReportInputFile(LobsterUISystemTestCaseBase):
+class LobsterHtmlReportInputFileTest(LobsterUISystemTestCaseBase):
     """System test case for LOBSTER HTML report with different .lobster
        file scenarios."""
 
@@ -11,7 +12,7 @@ class TestLobsterHtmlReportInputFile(LobsterUISystemTestCaseBase):
         self.test_runner = self.create_test_runner()
 
     def test_lobster_file_is_missing(self):
-        # lobster-trace: html_req.Missing_Lobster_File_Error
+        # lobster-trace: html_req.Missing_Lobster_File
         """Verify the tool fails gracefully when the .lobster file does not exist."""
         missing_lobster_file = self._data_directory / "missing.lobster"
         self.test_runner.cmd_args.lobster_report = str(missing_lobster_file)
@@ -27,11 +28,10 @@ class TestLobsterHtmlReportInputFile(LobsterUISystemTestCaseBase):
         )
 
         asserter.assertStdErrText(expected_stderr)
-        asserter.assertStdOutText("")
         asserter.assertExitCode(2)
 
     def test_valid_lobster_file_succeeds(self):
-        # lobster-trace: html_req.Valid_Lobster_File_Success
+        # lobster-trace: html_req.Valid_Lobster_File
         """Verify the tool runs successfully with a valid .lobster file."""
         output_filename = "is_actually_html.output"
         valid_inputfile = self._data_directory / "awesome.output"
@@ -43,14 +43,17 @@ class TestLobsterHtmlReportInputFile(LobsterUISystemTestCaseBase):
         completed_process = self.test_runner.run_tool_test()
         asserter = Asserter(self, completed_process, self.test_runner)
 
-        expected_stdout = (
-            "warning: dot utility not found, report will not include "
-            "the tracing policy visualisation\n"
-            "> please install Graphviz (https://graphviz.org)\n"
-            f"LOBSTER HTML report written to {output_filename}\n"
-        )
+        if is_dot_available(dot=None):
+            expected_stdout = f"LOBSTER HTML report written to {output_filename}\n"
 
-        asserter.assertStdErrText("")
+        else:
+            expected_stdout = (
+                "warning: dot utility not found, report will not include "
+                "the tracing policy visualisation\n"
+                "> please install Graphviz (https://graphviz.org)\n"
+                f"LOBSTER HTML report written to {output_filename}\n"
+            )
+
         asserter.assertStdOutText(expected_stdout)
         asserter.assertExitCode(0)
         asserter.assertOutputFiles()
