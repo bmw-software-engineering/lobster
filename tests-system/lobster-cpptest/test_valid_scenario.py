d23a2e32011671b83ae2d5edf778b583c85d67e1
Kedar Navare <kedar.na.navare@partner.bmwgroup.com>
2025-05-05 14:50:23 +0530

    Added tests cases for 2 scenarios, changes as per review comments

--- tests-system/lobster-cpptest/test_valid_scenario.py
+++ tests-system/lobster-cpptest/test_valid_scenario.py
@@ -8,12 +8,22 @@ class InputFileCpptestTest(LobsterCpptestSystemTestCaseBase):
         super().setUp()
         self._test_runner = self.create_test_runner()
         self._test_runner.declare_input_file(self._data_directory / "test_case.cpp")
-        self._test_runner.cmd_args.config = str(
-            self._data_directory / "cpptest-config.yaml")
 
     def test_valid_input_cpptest_file(self):
         # lobster-trace: cpptest_req.Input_File_Valid_Cpp_Test_File
+        self._test_runner.cmd_args.config = str(
+            self._data_directory / "cpptest-config.yaml")
+        completed_process = self._test_runner.run_tool_test()
+        asserter = Asserter(self, completed_process, self._test_runner)
 
+        asserter.assertNoStdErrText()
+        self.assertIn('lobster items to', completed_process.stdout)
+        asserter.assertExitCode(0)
+        asserter.assertOutputFiles()
+
+    def test_invalid_input_cpptest_file(self):
+        self._test_runner.cmd_args.config = str(
+            self._data_directory / "cpptest-config-invalid.yaml")
         completed_process = self._test_runner.run_tool_test()
         asserter = Asserter(self, completed_process, self._test_runner)
 
