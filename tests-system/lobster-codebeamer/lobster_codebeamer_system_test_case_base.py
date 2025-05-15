9a0e5f7c54e8d80b897fb195f5d1183ee44d91ab
SurajBDeore <suraj.da.deore@partner.bmwgroup.com>
2025-05-15 19:22:34 +0530

    Internal/lobster codebeamer mock server bug (#297)
    lobster codebeamer mock server bug resolved

--- tests-system/lobster-codebeamer/lobster_codebeamer_system_test_case_base.py
+++ tests-system/lobster-codebeamer/lobster_codebeamer_system_test_case_base.py
@@ -4,6 +4,7 @@ from ..system_test_case_base import SystemTestCaseBase
 
 
 class LobsterCodebeamerSystemTestCaseBase(SystemTestCaseBase):
+
     def __init__(self, methodName):
         super().__init__(methodName)
         self._data_directory = Path(__file__).parents[0] / "data"
@@ -15,3 +16,14 @@ class LobsterCodebeamerSystemTestCaseBase(SystemTestCaseBase):
             self.create_temp_dir(prefix=f"test-{tool_name}-"),
         )
         return test_runner
+
+    def set_config_file(self, retry_codes=None, num_retries=None):
+        cfg = self._test_runner.config_file_data
+        cfg.import_query = 1234458
+        cfg.root = "https://localhost:8999"
+        cfg.token = "abcdef1234567890"
+        cfg.out = "codebeamer.lobster"
+        if retry_codes is not None:
+            cfg.retry_error_codes = retry_codes
+        if num_retries is not None:
+            cfg.num_request_retry = num_retries
