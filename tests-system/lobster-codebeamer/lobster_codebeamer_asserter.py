34ad49be14956f13e66850909ea853823a9871db
Philipp Wullstein-Kammler <philipp.wullstein-kammler@bmw.de>
2025-05-26 10:14:56 +0200

    Request History for CB Mock Server
    The codebeamer mock server now stores a history of the received HTTP requests.
    This way it is now possible to test that `lobster-codebeamer` sends the correct requests.
    Previously it was only possible to test if the answers from the mock server have been
    digested properly by `lobster-codebeamer`, but it was not possible to verify that the
    initial requests sent to the server were correct.
    
    Updated test `test_extract_requirements_scenarios` to verify that `lobster-codebeamer`
    has sent the expected requests in the correct order with the correct page size parameter
    to the mock server.
    
    Also added an exception in case the system test framework tries to fetch the
    CB mock server instance before the server is aktually started.
    
    Introduced new make target to generate SSL certificates for the codebeamer mock server.
    This makes it handy to generate them on a developer client with just one command.
    
    Fixed type hints.
    
    Method `set_config_file_data` in class `LobsterCodebeamerSystemTestCaseBase` made the
    assumption that member variable `_test_runner` existed. The function has been deleted
    to resolve the issue. Instead, the class `ConfigFileData` now offers a member function
    to set default values for "root", "token" and "out".
    
    Specified what a "valid" codebeamer query ID is in requirement `codebeamer_req.Query_Id_Parameter`.

--- tests-system/lobster-codebeamer/lobster_codebeamer_asserter.py
+++ tests-system/lobster-codebeamer/lobster_codebeamer_asserter.py
@@ -1,19 +1,33 @@
 import math
+from typing import Optional
 from ..asserter import Asserter
 
 
 class LobsterCodebeamerAsserter(Asserter):
-    def assertStdOutNumAndFile(self, num_items: int = 1,
-                               out_file: str = "codebeamer.lobster",
-                               page_size: int = 1):
+    def assertStdOutNumAndFile(
+            self,
+            num_items: int,
+            page_size: int,
+            out_file: str = "codebeamer.lobster",
+            import_query: Optional[int] = None,
+    ):
         if num_items == 0:
+            if import_query is None:
+                self._test_case.fail("Invalid assertion call: If zero items are "
+                                     "expected then the import query must be provided!")
+            if not isinstance(import_query, int):
+                # The URL is constructed in a different way for string import_query
+                # and this function does not support this yet.
+                raise NotImplementedError("Having a string as import_query is not "
+                                          "supported yet by the test framework.")
             message = (
                 f"Fetching page 1 of query...\n"
                 f"This query doesn't generate items. Please check:\n"
                 f" * is the number actually correct?\n"
                 f" * do you have permissions to access it?\n"
                 f"You can try to access 'https://localhost:8999/api/v3/reports"
-                f"/1234458/items?page=1&pageSize={page_size}' manually to check.\n"
+                f"/{import_query}/items?page=1&pageSize={page_size}' manually to "
+                f"check.\n"
                 f"Written 0 requirements to {out_file}\n"
             )
         else:
