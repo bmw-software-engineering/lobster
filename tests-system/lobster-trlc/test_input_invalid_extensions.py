634d89b3105a2821c21f8e0e9100902ba410a550
Thomas Deboben <debobent@vispiron.de>
2025-07-09 15:10:14 +0200

    Single yaml config file for lobster-trlc.
    Single yaml config file for lobster-trlc.
    
    Goal is to remove the 'lobster-trlc.conf' beside the yaml config file.
    Remove supported config parameter 'trlc_config_file' holding the file name
    of the 'lobster-trlc.conf' config file.
    Add new supported config parameter 'trlc_config' to specify the trlc configuration
    directly inside the yaml config file using the block scalar style (|) for multi line strings.
    
    Example entry for the yaml config file:
    trlc_config: |
      package.typename {
        description = field_name
        tags "test" = field_name
      }

--- tests-system/lobster-trlc/test_input_invalid_extensions.py
+++ tests-system/lobster-trlc/test_input_invalid_extensions.py
@@ -6,8 +6,9 @@ class TrlcInvalidExtensionsTest(LobsterTrlcSystemTestCaseBase):
     def setUp(self):
         super().setUp()
         self._test_runner = self.create_test_runner()
-        self._test_runner.declare_trlc_config_file(self._data_directory /
-                                                   "lobster-trlc.conf")
+        config_string = self._test_runner.read_config_from_file(self._data_directory /
+                                                                "lobster-trlc.conf")
+        self._test_runner.declare_trlc_config(config_string)
 
     def test_invalid_extensions_inputs_files_list(self):
         # lobster-trace: trlc_req.Invalid_Inputs_List_Of_Files_Extensions
@@ -18,16 +19,17 @@ class TrlcInvalidExtensionsTest(LobsterTrlcSystemTestCaseBase):
         completed_process = self._test_runner.run_tool_test()
         asserter = Asserter(self, completed_process, self._test_runner)
         asserter.assertNoStdErrText()
-        asserter.assertStdOutText("<config>: lobster warning: not a .rsl or .trlc"
-                                  " file\n<config>: lobster warning: not a .rsl or"
-                                  " .trlc file\nrsl_invalid_extension.slr: error:"
-                                  " is not a rsl or trlc"
-                                  " file\ntrlc_invalid_extension.clrt: error: is not"
-                                  " a rsl or trlc file\ntest_default.namaste"
-                                  " {\n^^^^^^^^^^^^ lobster-trlc.conf:1: error:"
-                                  " unknown symbol test_default\nlobster-trlc:"
-                                  " aborting due to error in configuration file"
-                                  " 'lobster-trlc.conf'\n")
+        asserter.assertStdOutText(
+            "<config>: lobster warning: not a .rsl or .trlc file\n"
+            "<config>: lobster warning: not a .rsl or .trlc file\n"
+            "rsl_invalid_extension.slr: "
+            "error: is not a rsl or trlc file\n"
+            "trlc_invalid_extension.clrt: "
+            "error: is not a rsl or trlc file\n"
+            "test_default.namaste {\n"
+            "^^^^^^^^^^^^ :1: error: unknown symbol test_default\n"
+            "lobster-trlc: aborting due to error in configuration string "
+            "'test_default.namaste {\n  description = description\n}\n'\n")
         asserter.assertExitCode(1)
 
     def test_invalid_extensions_input_from_file(self):
@@ -38,16 +40,17 @@ class TrlcInvalidExtensionsTest(LobsterTrlcSystemTestCaseBase):
         completed_process = self._test_runner.run_tool_test()
         asserter = Asserter(self, completed_process, self._test_runner)
         asserter.assertNoStdErrText()
-        asserter.assertStdOutText("invalid_ext_inputs_from_file.txt:1: "
-                                  "lobster warning: not a .rsl or .trlc"
-                                  " file\ninvalid_ext_inputs_from_file.txt:2:"
-                                  " lobster warning: not a .rsl or"
-                                  " .trlc file\nrsl_invalid_extension.slr: error:"
-                                  " is not a rsl or trlc "
-                                  "file\ntrlc_invalid_extension.clrt: error: "
-                                  "is not a rsl or trlc file\ntest_default.namaste "
-                                  "{\n^^^^^^^^^^^^ lobster-trlc.conf:1: error: "
-                                  "unknown symbol test_default\nlobster-trlc:"
-                                  " aborting due to error in configuration file"
-                                  " 'lobster-trlc.conf'\n")
+        asserter.assertStdOutText(
+            "invalid_ext_inputs_from_file.txt:1: "
+            "lobster warning: not a .rsl or .trlc file\n"
+            "invalid_ext_inputs_from_file.txt:2: "
+            "lobster warning: not a .rsl or .trlc file\n"
+            "rsl_invalid_extension.slr: "
+            "error: is not a rsl or trlc file\n"
+            "trlc_invalid_extension.clrt: "
+            "error: is not a rsl or trlc file\n"
+            "test_default.namaste {\n"
+            "^^^^^^^^^^^^ :1: error: unknown symbol test_default\n"
+            "lobster-trlc: aborting due to error in configuration string "
+            "'test_default.namaste {\n  description = description\n}\n'\n")
         asserter.assertExitCode(1)
