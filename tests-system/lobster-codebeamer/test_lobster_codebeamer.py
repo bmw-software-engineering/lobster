af10137b15c98ab6ebde09f4b7fa4ae9e0931bac
Philipp Wullstein-Kammler <111539239+phiwuu@users.noreply.github.com>
2025-07-15 08:03:43 +0200

    Request History for CB Mock Server (#324)
    The codebeamer mock server now stores a history of the received HTTP
    requests.
    This way it is now possible to test that `lobster-codebeamer` sends the
    correct requests.
    Previously it was only possible to test if the answers from the mock
    server have been
    digested properly by `lobster-codebeamer`, but it was not possibly to
    verify that the
    initial requests sent to the server were correct.
    
    Updated test `test_extract_requirements_scenarios` to verify that
    `lobster-codebeamer`
    has sent the expected requests in the correct order with the correct
    page size parameter
    to the mock server.
    
    Also added an exception in case the system test framework tries to fetch
    the
    CB mock server instance before the server is aktually started.
    
    Introduced new make target to generate SSL certificates for the
    codebeamer mock server.
    This makes it handy to generate them on a developer client with just one
    command.
    
    Fixed type hints.
    
    Method `set_config_file_data` in class
    `LobsterCodebeamerSystemTestCaseBase` made the
    assumption that member variable `_test_runner` existed. The function has
    been deleted
    to resolve the issue. Instead, the class `ConfigFileData` now offers a
    member function
    to set default values for "root", "token" and "out".

--- tests-system/lobster-codebeamer/test_lobster_codebeamer.py
+++ tests-system/lobster-codebeamer/test_lobster_codebeamer.py
@@ -18,8 +18,8 @@ class LobsterCodebeamerTest(LobsterCodebeamerSystemTestCaseBase):
 
     def setUp(self):
         super().setUp()
+        self.codebeamer_flask.reset()
         self._test_runner = self.create_test_runner()
-        self.codebeamer_flask.responses = []
 
     def test_retry_if_configured(self):
         """Ensure the tool retries and exits after exhausting
@@ -27,7 +27,11 @@ class LobsterCodebeamerTest(LobsterCodebeamerSystemTestCaseBase):
         # lobster-trace: codebeamer_req.Retry_On_Specific_HTTPS_Status_Codes
 
         self.codebeamer_flask.responses = [Response(status=429)] * 3
-        self.set_config_file_data(retry_codes=[429], num_retries=3)
+        cfg = self._test_runner.config_file_data
+        cfg.set_default_root_token_out()
+        cfg.retry_error_codes = [429]
+        cfg.num_request_retry = 3
+        cfg.import_query = 999
 
         completed_process = self._test_runner.run_tool_test()
         asserter = Asserter(self, completed_process, self._test_runner)
@@ -36,8 +40,8 @@ class LobsterCodebeamerTest(LobsterCodebeamerSystemTestCaseBase):
             "[Attempt 1/3] Retryable error: 429\n"
             "[Attempt 2/3] Retryable error: 429\n"
             "[Attempt 3/3] Retryable error: 429\n"
-            f"Could not fetch {self._test_runner.config_file_data.root}/api/"
-            "v3/reports/1234458/items?page=1&pageSize=100.\n",
+            f"Could not fetch {cfg.root}/api/"
+            f"v3/reports/{cfg.import_query}/items?page=1&pageSize=100.\n",
             completed_process.stdout,
         )
         asserter.assertExitCode(1)
@@ -76,7 +80,11 @@ class LobsterCodebeamerTest(LobsterCodebeamerSystemTestCaseBase):
             Response(status=429),
             Response(json.dumps(response_data), status=200),
         ]
-        self.set_config_file_data(retry_codes=[429], num_retries=3)
+        cfg = self._test_runner.config_file_data
+        cfg.set_default_root_token_out()
+        cfg.retry_error_codes = [429]
+        cfg.num_request_retry = 3
+        cfg.import_query = 123123123123123123
         self._test_runner.declare_output_file(
             self._data_directory / self._test_runner.config_file_data.out)
 
@@ -96,7 +104,9 @@ class LobsterCodebeamerTest(LobsterCodebeamerSystemTestCaseBase):
         """Ensure the tool does NOT retry if RETRY_ERROR_CODES is not defined."""
         # lobster-trace: codebeamer_req.Missing_Error_Code
         self.codebeamer_flask.responses = [Response(status=429)]
-        self.set_config_file_data()
+        cfg = self._test_runner.config_file_data
+        cfg.set_default_root_token_out()
+        cfg.import_query = 1111
 
         completed_process = self._test_runner.run_tool_test()
         asserter = Asserter(self, completed_process, self._test_runner)
@@ -104,7 +114,7 @@ class LobsterCodebeamerTest(LobsterCodebeamerSystemTestCaseBase):
         self.assertIn(
             "[Attempt 1/5] Failed with status 429\n"
             f"Could not fetch {self._test_runner.config_file_data.root}/"
-            "api/v3/reports/1234458/items?page=1&pageSize=100.\n",
+            f"api/v3/reports/{cfg.import_query}/items?page=1&pageSize=100.\n",
             completed_process.stdout,
         )
         self.assertNotIn("Retrying request", completed_process.stdout)
