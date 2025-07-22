62c34bac1e4acac2a61a350c43a153a9931505dc
kedarnn <kedar.na.navare@partner.bmwgroup.com>
2025-07-22 15:53:10 +0530

    LOBSTER Release 0.13.1 (#354)

--- lobster/version.py
+++ lobster/version.py
@@ -18,7 +18,7 @@
 # <https://www.gnu.org/licenses/>.
 
 VERSION_TUPLE = (0, 13, 1)
-VERSION_SUFFIX = "dev"
+VERSION_SUFFIX = ""
 
 LOBSTER_VERSION = ".".join(str(x) for x in VERSION_TUPLE) + (
     "-%s" % VERSION_SUFFIX if VERSION_SUFFIX else ""
