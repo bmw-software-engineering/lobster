4bd89ff6426e9d141a26967591591d0b969934c7
Philipp Wullstein-Kammler <111539239+phiwuu@users.noreply.github.com>
2025-07-22 15:16:03 +0200

    Delete unit test `LobsterOnlineReportTests.test_valid_inputs` (#352)
    The unit test has been deleted because
    - it was essentially a system test (it called the `main()` function,
    checked its return value and its output to `stdout`),
    - and the system test `GitRepositoryTest.test_valid_git_repository`
    already covered it.
    
    This way the file `data/report-lobster.output` could also be deleted.
    
    A unit test shall not call external tools. Hence the call to `git` (to
    fetch the current commit SHA) has been replaced by a `unittest.patch`
    for function `online_report.get_hash_for_git_commit`.

--- tests-unit/lobster-online-report/test_online_report.py
+++ tests-unit/lobster-online-report/test_online_report.py
@@ -1,40 +1,15 @@
-import json
-import pathlib
-import sys
 import unittest
-from contextlib import redirect_stdout
-from io import StringIO
-from os.path import dirname
 from pathlib import Path
-import subprocess
+from unittest.mock import patch
 
 from lobster.items import Item, Tracing_Tag
 from lobster.location import File_Reference
 from lobster.tools.core.online_report.online_report import (
-    main, get_summary, get_git_commit_hash_repo_and_path
+    get_summary, get_git_commit_hash_repo_and_path
 )
 
 
 class LobsterOnlineReportTests(unittest.TestCase):
-    def test_valid_inputs(self):
-        input_file = str(Path(dirname(__file__), "data",
-                                   "report-lobster.output"))
-        online_report = "online-report.lobster"
-
-        sys.argv = ["lobster-online-report", input_file,
-                    f'--out={online_report}']
-        with StringIO() as stdout, redirect_stdout(stdout):
-            exit_code = main()
-        with open(online_report, 'r', encoding="UTF-8") as file:
-            data = json.load(file)
-            for level in data['levels']:
-                for item in level['items']:
-                    with self.subTest(item):
-                        location = item['location']
-                        if 'file' in location:
-                            self.assertIsNotNone(location.get('commit'))
-        self.assertEqual(exit_code, 0)
-
     def test_print_summary_same_values(self):
         value = "marble"
         actual = get_summary(value, value)
@@ -52,21 +27,21 @@ class LobsterOnlineReportTests(unittest.TestCase):
             f"LOBSTER report {out_file} created, using online references.",
         )
 
-    def test_commit_hash_for_main_repo(self):
-        root = " https://github.com/bmw-software-engineering/lobster"
-        submodule_roots = {}
-        repo_root = pathlib.Path().cwd()
-        git_hash_cache = {}
-        location = File_Reference("/data/basic.py")
-        tag = Tracing_Tag("test_namespace", "python basic.trlc_reference")
-        item = Item(tag, location)
-        expected_commit = subprocess.check_output(
-            ["git", "rev-parse", "HEAD"],
-            universal_newlines=True, cwd=repo_root
-        ).strip()
+    @patch('lobster.tools.core.online_report.online_report.get_hash_for_git_commit')
+    def test_commit_hash_for_main_repo(self, mock_get_hash):
+        mock_get_hash.return_value = "mocked_commit_hash"
+        item = Item(
+            Tracing_Tag("test_namespace", "ÄÖÜ"),
+            File_Reference("123.def"),
+        )
         _, _, commit = get_git_commit_hash_repo_and_path(
-            root, submodule_roots, item, repo_root, git_hash_cache)
-        self.assertEqual(expected_commit, commit)
+            gh_root="abc",
+            gh_submodule_roots={},
+            item=item,
+            repo_root=Path(__file__).parent,
+            git_hash_cache={},
+        )
+        self.assertEqual("mocked_commit_hash", commit)
 
 if __name__ == '__main__':
     unittest.main()
