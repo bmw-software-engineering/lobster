e71f1856376b9fd4135032337bd5491940cffa75
Philipp Wullstein-Kammler <111539239+phiwuu@users.noreply.github.com>
2025-07-28 08:40:23 +0200

    Check output of `lobster-ci-report` in integration test (#363)
    The `Makefile` for all the integration tests call `lobster-ci-report`,
    but never compare its output against an expectation.
    
    This commit fixes the problem in the "basic" and "cpp-focus" integration
    tests. Other tests are not fixed.
    
    Also adjusted file endings to be `*.lobster`, if they are LOBSTER files.
    
    Also updated the script which replaces the placeholder in a LOBSTER file
    with the current commit hash such that a new file is created instead of
    overwriting the existing file.

--- tests-system/tests_utils/update_online_json_with_hashes.py
+++ tests-system/tests_utils/update_online_json_with_hashes.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 
 
-def update_json(filename, expected_location=None):
+def update_json(filename, expected_location=None, out=None):
     # Load the JSON data from the file
     with open(filename, 'r') as file:
         data = json.load(file)
@@ -19,13 +19,15 @@ def update_json(filename, expected_location=None):
                 if expected_location:
                     location['file'] = expected_location
 
-    # Save the updated JSON data back to the same file
-    with open(filename, 'w') as fd:
+    # Save the updated JSON data into the output file
+    if not out:
+        out = filename
+    with open(out, 'w') as fd:
         json.dump(data, fd, indent=2)
         fd.write("\n")
 
-    print(f"JSON data updated and saved back to '{filename}'.")
+    print(f"JSON data updated and saved to '{out}'.")
 
 
 if __name__ == "__main__":
-    update_json(sys.argv[1])
+    update_json(sys.argv[1], out=sys.argv[2])
