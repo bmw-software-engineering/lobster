fe8f51243eac6ff9bdb5c3e70f1dab0da15d3f58
suraj <Suraj.DA.Deore@bti.bmwgroup.com>
2025-08-18 18:04:22 +0530

    Add coverage rule for message-traced items
    - The item with the message shall have 0% coverage.
    - The traced-to item remains at 100% coverage
    Issue: SWF-20649

--- tests_system/lobster_report/test_items_coverage.py
+++ tests_system/lobster_report/test_items_coverage.py
@@ -3,7 +3,7 @@ from tests_system.lobster_report.lobster_report_system_test_case_base import (
     LobsterReportSystemTestCaseBase)
 
 
-class ReportZeroItemsCoverageTest(LobsterReportSystemTestCaseBase):
+class ReportItemsCoverageTest(LobsterReportSystemTestCaseBase):
     def setUp(self):
         super().setUp()
         self._test_runner = self.create_test_runner()
@@ -20,6 +20,28 @@ class ReportZeroItemsCoverageTest(LobsterReportSystemTestCaseBase):
         conf_file = "lobster_zero_items.conf"
         out_file = "report_zero_items.lobster"
         self._test_runner.cmd_args.lobster_config = conf_file
+        self._test_runner.cmd_args.out = out_file
+        self._test_runner.declare_output_file(self._data_directory / out_file)
+
+        completed_process = self._test_runner.run_tool_test()
+
+        asserter = Asserter(self, completed_process, self._test_runner)
+        asserter.assertNoStdErrText()
+        asserter.assertNoStdOutText()
+        asserter.assertExitCode(0)
+        asserter.assertOutputFiles()
+
+    def test_items_message_trace_coverage(self):
+        # lobster-trace: core_report_req.Message_Trace_Coverage
+        self._test_runner.declare_input_file(self._data_directory /
+                                             "message_trace_coverage.conf")
+        self._test_runner.declare_input_file(self._data_directory /
+                                             "python_message_trace_coverage.lobster")
+        self._test_runner.declare_input_file(self._data_directory /
+                                             "trlc_message_trace_coverage.lobster")
+
+        out_file = "report_message_trace_coverage.lobster"
+        self._test_runner.cmd_args.lobster_config = "message_trace_coverage.conf"
         self._test_runner.cmd_args.out = out_file
         self._test_runner.declare_output_file(self._data_directory / out_file)
 
