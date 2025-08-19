4bc6bb1593fee69f5988231b8812c5be592e7908
Thomas Deboben <debobent@vispiron.de>
2025-08-19 11:23:15 +0200

    fix new trlc system tests

--- tests_system/lobster_trlc/test_conversion.py
+++ tests_system/lobster_trlc/test_conversion.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from dataclasses import dataclass
-from .lobster_system_test_case_base import LobsterTrlcSystemTestCaseBase
-from ..asserter import Asserter
+from tests_system.lobster_trlc.lobster_system_test_case_base import LobsterTrlcSystemTestCaseBase
+from tests_system.asserter import Asserter
 
 
 @dataclass
