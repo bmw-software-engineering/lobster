5c73dbdf6c2668418a0bc0b1fb67cf4f63cb9547
Philipp Wullstein-Kammler <111539239+phiwuu@users.noreply.github.com>
2025-09-09 12:36:52 +0200

    `lobster-pkg`: use cases, potential errors and system tests (#475)
    I recommend to merge
    https://github.com/bmw-software-engineering/lobster/pull/474 first, and
    then rebase the current branch onto `main`.
    
    For `lobster-pkg`:
    - add system requirements
    - add potential errors
    - add system test case to cover all potential errors
    
    Also align the formulation between the potential errors of the tools
    `lobster-pkg` and `lobster-trlc` such that they both use the term "tool"
    instead of the actual tool name. This way we can potentially unify the
    potential errors in the future in order to reduce maintenance effort.

--- tests_system/lobster_pkg/test_valid_scenario.py
+++ tests_system/lobster_pkg/test_valid_scenario.py
@@ -12,6 +12,10 @@ class InputFilePkgTest(LobsterPKGSystemTestCaseBase):
         self._test_runner = self.create_test_runner()
 
     def test_valid_input_pkg_file(self):
+        # lobster-trace: UseCases.PKG_Items_Extraction
+        # lobster-trace: req.Pkg_File_Item
+        # lobster-trace: req.Trace_in_Test_Step_Node
+        # lobster-trace: req.Trace_in_Analysis_Node
         OUT_FILE = "valid_file1.lobster"
         self._test_runner.declare_input_file(self._data_directory / "valid_file1.pkg")
         self._test_runner.cmd_args.files = ["valid_file1.pkg"]
@@ -29,6 +33,9 @@ class InputFilePkgTest(LobsterPKGSystemTestCaseBase):
         asserter.assertOutputFiles()
 
     def test_valid_input_ta_file(self):
+        # lobster-trace: UseCases.PKG_Items_Extraction
+        # lobster-trace: req.Pkg_File_Item
+        # lobster-trace: req.Trace_in_Analysis_Node
         OUT_FILE = "valid_ta_file.lobster"
         self._test_runner.declare_input_file(self._data_directory / "valid_file.ta")
         self._test_runner.cmd_args.files = ["valid_file.ta"]
@@ -46,6 +53,10 @@ class InputFilePkgTest(LobsterPKGSystemTestCaseBase):
         asserter.assertOutputFiles()
 
     def test_valid_ta_file_with_misplaced_traces(self):
+        # lobster-trace: UseCases.PKG_Items_Extraction
+        # lobster-trace: UseCases.Warning_for_Misplaced_Trace
+        # lobster-trace: req.Pkg_File_Item
+        # lobster-trace: req.Misplaced_Traces
         OUT_FILE = "with_misplaced_traces.lobster"
         self._test_runner.cmd_args.files = [
             str(self._data_directory / "with_misplaced_traces.ta")
@@ -69,6 +80,10 @@ class InputFilePkgTest(LobsterPKGSystemTestCaseBase):
         asserter.assertExitCode(0)
 
     def test_valid_pkg_file_with_misplaced_traces(self):
+        # lobster-trace: UseCases.PKG_Items_Extraction
+        # lobster-trace: UseCases.Warning_for_Misplaced_Trace
+        # lobster-trace: req.Pkg_File_Item
+        # lobster-trace: req.Misplaced_Traces
         OUT_FILE = "valid_file1.lobster"
         self._test_runner.cmd_args.files = [
             str(self._data_directory / "with_misplaced_traces.pkg")
@@ -92,6 +107,10 @@ class InputFilePkgTest(LobsterPKGSystemTestCaseBase):
         asserter.assertExitCode(0)
 
     def test_valid_input_pkg_files(self):
+        # lobster-trace: UseCases.PKG_Items_Extraction
+        # lobster-trace: req.Pkg_File_Item
+        # lobster-trace: req.Trace_in_Test_Step_Node
+        # lobster-trace: req.Trace_in_Analysis_Node
         OUT_FILE = "valid_file1_and_valid_file2.lobster"
         for file in ("valid_file1.pkg", "valid_file2.pkg"):
             self._test_runner.declare_input_file(self._data_directory / file)
@@ -110,7 +129,35 @@ class InputFilePkgTest(LobsterPKGSystemTestCaseBase):
         asserter.assertExitCode(0)
         asserter.assertOutputFiles()
 
+    def test_valid_input_file_and_extra_file(self):
+        """Test that an extra file in the working directory is ignored."""
+        # lobster-trace: UseCases.PKG_Items_Extraction
+        # lobster-trace: req.Pkg_File_Item
+        # lobster-trace: req.Trace_in_Test_Step_Node
+        # lobster-trace: req.Trace_in_Analysis_Node
+        OUT_FILE = "valid_file1.lobster"
+        IN_FILES = ("valid_file1.pkg", "valid_file2.pkg")
+        for f in IN_FILES:
+            self._test_runner.copy_file_to_working_directory(self._data_directory / f)
+        self._test_runner.cmd_args.files.append(IN_FILES[0])
+
+        out_file = self._data_directory / OUT_FILE
+        self._test_runner.declare_output_file(out_file)
+
+        self._test_runner.cmd_args.out = OUT_FILE
+        completed_process = self._test_runner.run_tool_test()
+
+        asserter = LobsterPkgAsserter(self, completed_process, self._test_runner)
+        asserter.assertNoStdErrText()
+        asserter.assertStdOutNumAndFile(1, OUT_FILE)
+        asserter.assertExitCode(0)
+        asserter.assertOutputFiles()
+
     def test_valid_input_pkg_folder(self):
+        # lobster-trace: UseCases.PKG_Items_Extraction
+        # lobster-trace: req.Pkg_File_Item
+        # lobster-trace: req.Trace_in_Test_Step_Node
+        # lobster-trace: req.Trace_in_Analysis_Node
         OUT_FILE = "valid_file1_folder.lobster"
 
         pkg_files_dir = Path(self._test_runner.working_dir) / "pkg_files"
@@ -135,6 +182,8 @@ class InputFilePkgTest(LobsterPKGSystemTestCaseBase):
         asserter.assertOutputFiles()
 
     def test_valid_file_without_lobster_trace(self):
+        # lobster-trace: req.Pkg_File_Item
+        # lobster-trace: UseCases.PKG_Items_Extraction
         OUT_FILE = "without_lobster_trace.lobster"
         self._test_runner.declare_input_file(self._data_directory /
                                              "without_lobster_trace.pkg")
@@ -153,6 +202,8 @@ class InputFilePkgTest(LobsterPKGSystemTestCaseBase):
         asserter.assertOutputFiles()
 
     def test_valid_file_without_testcase_tag(self):
+        # lobster-trace: req.Pkg_File_Item
+        # lobster-trace: UseCases.PKG_Items_Extraction
         OUT_FILE = "without_testcase_tag.lobster"
         self._test_runner.declare_input_file(self._data_directory /
                                              "without_testcase_tag.pkg")
