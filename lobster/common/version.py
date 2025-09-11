9d0438a1f0e725ab6c5362b314eee87e10dd5b0b
Philipp Wullstein-Kammler <111539239+phiwuu@users.noreply.github.com>
2025-09-11 07:55:34 +0200

    Bump to 1.0.1-dev (#491)
    Bump version after release

--- lobster/common/version.py
+++ lobster/common/version.py
@@ -17,8 +17,8 @@
 # License along with this program. If not, see
 # <https://www.gnu.org/licenses/>.
 
-VERSION_TUPLE = (1, 0, 0)
-VERSION_SUFFIX = ""
+VERSION_TUPLE = (1, 0, 1)
+VERSION_SUFFIX = "dev"
 
 LOBSTER_VERSION = ".".join(str(x) for x in VERSION_TUPLE) + (
     "-%s" % VERSION_SUFFIX if VERSION_SUFFIX else ""
